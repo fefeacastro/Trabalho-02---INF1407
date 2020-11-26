5715269285bee49298398a7ea0890a23ecc66fe3
Fernanda Castro <fefeacastro@hotmail.com>
2020-11-26 12:25:00 -0300

    Adicionando página de sapatos e vestidos

--- LojaMariFe/project/views.py
+++ LojaMariFe/project/views.py
@@ -36,6 +36,15 @@ def camisa(request):
 
 def calca(request):
     calcas = Produto.objects.filter(categoria = 'Calça')
-    print(calcas)
     context = {'calcas':calcas,}
     return render(request, 'LojaMariFe/calca.html', context=context)
+
+def sapato(request):
+    sapatos = Produto.objects.filter(categoria = 'Sapato')
+    context = {'sapatos':sapatos,}
+    return render(request, 'LojaMariFe/sapato.html', context=context)
+
+def vestido(request):
+    vestidos = Produto.objects.filter(categoria = 'Vestido')
+    context = {'vestidos':vestidos,}
+    return render(request, 'LojaMariFe/vestido.html', context=context)
