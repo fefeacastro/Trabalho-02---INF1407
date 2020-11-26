d421b95cffea6cf0dc5d72f071626e70c2fdfe9d
Mariana Salgueiro <marianadsalgueiro@gmail.com>
2020-11-26 18:42:57 -0300

    ajeitando caminho das imagens do banco

--- LojaMariFe/project/models.py
+++ LojaMariFe/project/models.py
@@ -5,7 +5,7 @@ class Produto(models.Model):
     descricao = models.CharField(max_length=250, help_text='Descrição do produto')
     preco = models.DecimalField(decimal_places=2, max_digits=10, help_text='Preço do produto')
     quantidade = models.IntegerField(help_text='Quantidade de produto em estoque')
-    imagem = models.ImageField(help_text='Imagem do produto')
+    imagem = models.ImageField(help_text='Imagem do produto', upload_to='project/static/images/')
 
     choices = (("Camisa", "Camisa"),
         ("Calça", "Calça"),
